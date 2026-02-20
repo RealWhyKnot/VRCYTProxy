82335cf1b3dc9e665cc1ae36adab4a5ca4288066
WhyKnot <github.idealness@pass.webmailroom.net>
2026-02-20 13:17:31 -0600

    v2026.02.20.2

--- src/patcher/main.py
+++ src/patcher/main.py
@@ -26,7 +26,7 @@ try:
     from _version import __version__ as CURRENT_VERSION
     from _version import __build_type__ as BUILD_TYPE
 except ImportError:
-    CURRENT_VERSION = "v2026.02.20.dev-main-2829d98"
+    CURRENT_VERSION = "v2026.02.20.dev-main-b9d0f34"
     BUILD_TYPE = "DEV"
 
 GITHUB_REPO_OWNER = "RealWhyKnot"
